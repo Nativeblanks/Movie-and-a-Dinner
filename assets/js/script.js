b49fe5b838f640b5c8740163e7199fd0ed01acf6
Zach B <nativeblanks@gmail.com>
2021-09-13 20:29:25 -0400

    random meal added

---
+++ assets/js/script.js
@@ -0,0 +1,61 @@
+
+const get_meal_btn = document.getElementById('get_meal');
+const meal_container = document.getElementById('meal');
+
+
+get_meal_btn.addEventListener('click', () => {
+  fetch('https://www.themealdb.com/api/json/v1/1/random.php')
+    .then(response => response.json())
+    .then(response => {
+    createMeal(response.meals[0]);
+  });
+});
+
+const createMeal = (meal) => {
+  const ingredients = [];
+  // Get all ingredients from the object. Up to 20
+  for(let i=1; i<=20; i++) {
+    if(meal[`strIngredient${i}`]) {
+      ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`)
+    } else {
+      // Stop if no more ingredients
+      break;
+    }
+  }
+  
+  const newInnerHTML = `
+    <div class="row">
+      <div class="column">
+        <img src="${meal.strMealThumb}" alt="Meal">
+        <div>
+        ${meal.strCategory ? `<p><strong>Category:</strong> ${meal.strCategory}</p>` : ''}
+        ${meal.strArea ? `<p><strong>Area:</strong> ${meal.strArea}</p>` : ''}
+        ${meal.strTags ? `<p><strong>Tags:</strong> ${meal.strTags.split(',').join(', ')}</p>` : ''}
+        <h5>Ingredients:</h5>
+       </div>
+       <div>
+        <ul>
+          ${ingredients.map(ingredient => `
+          <li>${ingredient}</li>`).join('')}
+        </ul>
+        </div>
+      </div>
+      <div class="column">
+        <h5>${meal.strMeal}</h5>
+        <br>
+        <p>${meal.strInstructions}</p>
+      </div>
+    </div>
+    ${meal.strYoutube ? `
+    <div class="row">
+      <h5>Video Recipe</h5>
+      <div class="videoYT">
+        <iframe width="420" height="315"
+        src="https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}">
+        </iframe>
+      </div>
+    </div>` : ''}
+  `;
+  
+  meal_container.innerHTML = newInnerHTML;
+}
